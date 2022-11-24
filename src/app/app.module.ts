a53c9cbaac78a3ffd4978558335c050aa2851f5e
AmalAlby <amalalbyn@gmail.com>
2022-11-24 16:06:51 +0530

    admin login created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminloginComponent } from './adminlogin/adminlogin.component';
+import { Route, RouterModule, Routes } from '@angular/router';
+
+
+const myroute:Routes=[
+  {
+    path:"",
+    component:AdminloginComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminloginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myroute)
   ],
   providers: [],
   bootstrap: [AppComponent]
